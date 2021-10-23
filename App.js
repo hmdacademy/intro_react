496d474a2d719deeb1774674378696db346ddf3b
rahmatoktavian <rahmatoktavian@gmail.com>
2021-10-23 10:40:42 +0700

    update

--- App.js
+++ App.js
@@ -6,14 +6,20 @@ export default function App() {
   return (
     <PaperProvider>
       <Appbar.Header>
-        <Appbar.Content title="Title" subtitle={'Subtitle'} />
+        <Appbar.BackAction onPress={() => alert('back')} />
+        <Appbar.Content title="HMD Academy" />
         <Appbar.Action icon="delete" onPress={() => alert('Delete')} />
+        <Appbar.Action icon="map-marker" onPress={() => alert('Tambah')} />
       </Appbar.Header>
 
       <Text style={styles.text}>Hello world</Text>
 
       <Button icon="cursor-default" mode="contained" style={styles.button} onPress={() => alert('Insert')}>
         Click Me
+      </Button>
+
+      <Button icon="camera" mode="contained" style={{margin:20, backgroundColor:'red'}} onPress={() => console.log('Pressed')}>
+        Press me
       </Button>
     </PaperProvider>
   );
